74f6afd2e28c32be0aaec4d62614e0a79e2b38d2
Mykyta Dniprovskyi <dnipromv@gmail.com>
2019-12-07 13:14:38 +0100

    implemented tile generation

---
+++ Assets/Level.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public TextAsset levelDataJSON;
+    public GameObject regularTile;
+    private LevelData levelData;
+
+    private void Awake() {
+        levelData = JsonUtility.FromJson<LevelData>(levelDataJSON.ToString());
+
+        foreach(TileData tileData in levelData.planes[0].tiles) {
+            GameObject tile = Object.Instantiate(regularTile, this.gameObject.transform);
+            tile.transform.position = new Vector3(tileData.column * 1.05f, 0, tileData.row * 1.05f);
+        }
+    }
+}
---
+++ Assets/LevelData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelData
+{
+    public List<PlaneData> planes;
+}
+
+[System.Serializable]
+public class PlaneData 
+{
+    public int id;
+    public int width;
+    public int height;
+    public List<TileData> tiles;
+}
+
+[System.Serializable]
+public class TileData
+{
+    public int id;
+    public int row;
+    public int column;
+    public string type;
+    public string side;
+}
